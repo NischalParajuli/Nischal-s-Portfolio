59a8f6809ab8d85e02e80bab039f4674237d4f7d
NischalParajuli <parajulinischal204@gmail.com>
2026-05-21 22:36:53 +0545

    Added Statics

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -9,7 +9,3 @@ urlpatterns = [
     path('', views.home, name='home'), 
 ]
 
-# Serve static files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
